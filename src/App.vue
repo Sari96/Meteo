b87d87b1e1dce286913954c686b5680fd715d384
Sarah Cappelli <sarahcappelli96@gmail.com>
2023-06-02 08:33:44 +0200

    First working version with Vue

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script>
+import DateButton from './components/DateButton.vue'
+
+export default {
+  data() {
+    return {
+      cityName: "",
+      dailyData: {},
+    };
+  },
+  components: {
+    DateButton,
+  },
+  methods: {
+    getWeatherImg(code, is_day) {
+      var img;
+      if (code == 0)
+        img = "clear";
+      else if ([1, 2, 3].includes(code))
+        img = "partly_cloudly";
+      else if ([45, 48].includes(code))
+        img = "foggy";
+      else if ([51, 53, 55, 56, 57, 61, 66, 80].includes(code))
+        img = "drizzle";
+      else if ([63, 65, 67, 81, 82].includes(code))
+        img = "rain";
+      else if ([71, 73, 75, 77, 85, 86].includes(code))
+        img = "snow";
+      else if ([95,96, 99].includes(code))
+        img = "thunder";
+
+      img = new URL("../images/" + img + "_" + is_day + ".svg", import.meta.url).href;
+
+      return img;
+    },
+
+    loadCity(lat, lng) {
+      this.url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&`;
+      fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lng}&appid=ca0bc81789a77b485742ed7b77c9a1b9&limit=0`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.cityName = data[0].name;
+        this.loadDailyData(lat, lng);
+      });
+    },
+
+    loadDailyData(lat, lng) {
+      const dailyUrl = this.url + 'timezone=auto&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum,weathercode,sunrise,sunset';
+      console.log(dailyUrl);
+      fetch(dailyUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const tmp = data.daily.time.map((time, index) => ({
+          date: new Date(time),
+          temperature_2m_avg: Math.round((data.daily.temperature_2m_max[index] + data.daily.temperature_2m_min[index]) / 2),
+          //apparent_temperature_max: data.daily.apparent_temperature_max[index],
+          //apparent_temperature_min: data.daily.apparent_temperature_min[index],
+          //precipitation_sum: data.daily.precipitation_sum[index],
+          //weathercode: data.daily.weathercode[index],
+          sunrise: data.daily.sunrise[index],
+          sunset: data.daily.sunset[index],
+          svg: this.getWeatherImg(data.daily.weathercode[index], 1)
+        }));
+        data.daily = tmp;
+        this.dailyData = data;
+        //   const svg = ;
+      });
+    }
+  },
+  beforeMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        this.loadCity(lat, lng);
+    });
+  }
+};
+</script>
+
+<template>
+  <main>
+    <div class="container mt-3">
+      <h1 class="text-center">{{ cityName }}</h1>
+      <div id="dateBar" class="my-5 card-group btn-group d-flex justify-content-evenly" role="group" aria-label="Basic radio toggle button group">
+        <DateButton v-for="(item, index) in dailyData.daily"
+        :data="item"
+        :index="index"
+        :measures="dailyData.daily_units"
+        />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/DateButton.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineProps({
+  data: {
+    required: true,
+    type: Object
+  },
+  index: {
+    required: true,
+    type: Number
+  },
+  measures: {
+    required: true,
+    type: Object
+  }
+})
+</script>
+
+<template>
+<label>
+  <input type="radio" class="btn-check" name="btnradio" :id="'btnradio-' + data.date.getDate()" autocomplete="off" :onclick="'setWeatherDate(' + index + ')'">
+  <div class="card d-flex flex-column justify-content-between">
+    <div class="card-body">
+      <h5 class="card-title">{{data.temperature_2m_avg + ' ' + measures.apparent_temperature_max}}</h5>
+      <img :src="data.svg"/>
+      <h5 class="card-title day">{{data.date.toLocaleDateString('it-IT', {weekday: 'short'})}}</h5>
+    </div>
+  </div>
+</label>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/main';
+.card {
+		@extend %glass;
+		text-align: center;
+		top: auto;
+		height: 100%;
+
+		&.selected {
+			background: rgba(255, 255, 255, 0.75);
+		}
+
+		img {
+			width: 5vw;
+			height: 10vh;
+		}
+
+		.card-title.day {
+			text-transform: capitalize;
+		}
+}
+</style>
